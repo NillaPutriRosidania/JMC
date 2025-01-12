4582bf63adf7739018bc4584f42b53b8a3f968be
Nilla Putri Rosidania <nillaputrirosidania@gmail.com>
2025-01-12 21:54:45 +0700

    pre - k_means feature

---
+++ resources/views/kmeans_akb/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('title', 'Clustering AKB')
+
+@section('content')
+@endsection
---
+++ resources/views/kmeans_aki/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('title', 'Clustering AKI')
+
+@section('content')
+@endsection
